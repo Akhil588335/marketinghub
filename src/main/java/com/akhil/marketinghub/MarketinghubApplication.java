1012e7ba51e6cd035f068b877d43e6c1582fe3b0
Akhil588335 <akhilbandi077@gmail.com>
2026-07-01 15:03:31 +0530

    Week 0: Initial Spring Boot project setup with PostgreSQL connection

---
+++ src/main/java/com/akhil/marketinghub/MarketinghubApplication.java
@@ -0,0 +1,13 @@
+package com.akhil.marketinghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketinghubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketinghubApplication.class, args);
+    }
+
+}
